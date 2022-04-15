267338cd665f917531efd6945c0f4a430fbe0ffa
Violette <1372597331@qq.com>
2022-04-15 09:48:15 +0800

    coding

--- ♥5. 最长回文子串.cpp
+++ ♥5. 最长回文子串.cpp
@@ -5,39 +5,38 @@ using namespace std;
 //注意dp[i][j]，表示s[i...j]是否为回文串，而不是s[i...j]中的最长回文串，额外维护变量来记录最长回文串
 class Solution {
 public:
-    string longestPalindrome(string s){
-        int left = 0;
-        int right = s.size() - 1;
+    string longestPalindrome(string s) {
         vector<vector<bool>> dp(s.size(), vector<bool>(s.size()));
         for (int i = 0; i < s.size(); i++) {
             dp[i][i] = true;
         }
         int ret = INT_MIN;
         int len = s.size();
-        string ret_str = "";
+        //不用维护结果字串，维护左右边界即可
+        //string ret_str = "";
+        int left = 0;
+        int right = 0;
         //刚开始的填表方式不太对，导致引用了没有设定过的值
         //填表方式可以很灵活，只需要保证不引用没有设定过的值
-        for (int i = len -1; i >=0; i--) {
+        for (int i = len - 1; i >= 0; i--) {
             for (int j = len - 1; j > i; j--) {
-
-                if ((j == (i + 1)) && s[i] == s[j]) {
-                    dp[i][j] = true;
-                }
-                else if (s[i] == s[j] && j - i + 1 > 2) {
-                    dp[i][j] = dp[i + 1][j - 1];
-                }
-                else if (s[i] != s[j]) {
+                //字符不等
+                if (s[i] != s[j]) {
                     dp[i][j] = false;
                 }
-
-
+                else if (j - i + 1 <= 2) { //字符相等，长度为2，是回文串
+                    dp[i][j] = true;
+                }
+                else dp[i][j] = dp[i + 1][j - 1]; //字符相等，长度超过2，依赖于内部是不是回文串
+                //更新
                 if (dp[i][j] && j - i + 1 > ret) {
                     ret = j - i + 1;
-                    ret_str = s.substr(i, j - i + 1);
+                    left = i;
+                    right = j;
                 }
             }
         }
-        return ret_str == "" ? s.substr(0, 1) : ret_str;
+        return s.substr(left, right - left + 1);
     }
 };
 
